b52feac2ff53efbc07692a26c6b0e1d58855c317
Peter Chang <Tokenekie@gmail.com>
2010-05-31 16:44:28 -0700

    Added buffer overflow check in task_upload()
    Added buffer overflow checks for file name size in task_upload()

--- osppeer.c
+++ osppeer.c
@@ -674,6 +674,10 @@ static void task_upload(task_t *t)
 	}
 
 	assert(t->head == 0);
+   if ((t->tail - strlen("GET  OSP2P\n")) > FILENAMESIZ) {
+      error("* Peer filename buffer overrun error");
+      goto exit;
+   }
 	if (osp2p_snscanf(t->buf, t->tail, "GET %s OSP2P\n", t->filename) < 0) {
 		error("* Odd request %.*s\n", t->tail, t->buf);
 		goto exit;
